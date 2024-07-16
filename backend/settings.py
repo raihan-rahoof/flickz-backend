6ef06e6b34665c6a52117ba4b61ea07f533d143c
raihan-rahoof <mcrahoof28@gmail.com>
2024-07-16 11:22:59 +0530

    again commited email issue

--- backend/settings.py
+++ backend/settings.py
@@ -201,7 +201,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # EMAIL_HOST_PASSWORD = "9df82fb38c9fcc"
 # EMAIL_PORT = "2525"
 # EMAIL_USE_TLS = True
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
