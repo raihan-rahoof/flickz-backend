26101e3a7555974c15104945fac56ccf2cefcb8f
raihan-rahoof <mcrahoof28@gmail.com>
2024-10-07 19:40:12 +0530

    review system full azhichupani

--- users/models.py
+++ users/models.py
@@ -3,11 +3,3 @@ from adminside.models import Movie
 from user_auth.models import User
 
 # Create your models here.
-class Review(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='reviews')
-    movie = models.ForeignKey(Movie,on_delete=models.CASCADE,related_name='reviews')
-    review_text = models.TextField()
-    sentiment = models.CharField(max_length=25,null=True,blank=True)
-
-    def __str__(self) -> str:
-        return f"Review of {self.movie.title} by {self.user.first_name}"
--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("movies/", HomeMovieListView.as_view(), name="home-movie-list"),
     path("movie/<int:pk>/", MovieDetailsView.as_view(), name="movie-detail"),
     path("movies/search/", MovieSearchView.as_view(), name="movie-search"),
-    path('movies/<int:movie_id>/reviews/',ReviewCreateView.as_view(),name='reviews')
+    
 ]
