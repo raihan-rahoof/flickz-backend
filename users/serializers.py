b910bd8ab8abfa7608f26d10c2d144f6093c1234
raihan-rahoof <mcrahoof28@gmail.com>
2024-10-07 19:53:28 +0530

    removed full review section

--- users/serializers.py
+++ users/serializers.py
@@ -1,28 +1,10 @@
 from rest_framework import serializers
 from adminside.models import Movie
 from user_auth.models import User
-from .models import Review
+
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
-class UserForReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id','first_name','last_name','email']
-
-class MovieForReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ['id','title','language']
-
-class ReviewSerializer(serializers.ModelSerializer):
-    user = UserForReviewSerializer
-    movie = MovieForReviewSerializer
-    booking_id = serializers.IntegerField()
-
-    class Meta:
-        model = Review
-        fields = ['id','user','movie','review_text','sentiment','booking_id']
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeMovieListView , MovieDetailsView , MovieSearchView ,ReviewCreateView
+from .views import HomeMovieListView , MovieDetailsView , MovieSearchView 
 
 
 urlpatterns = [
